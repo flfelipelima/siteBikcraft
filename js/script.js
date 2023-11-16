86e06bfbcb7c129321315ad721d5cc247c08850f
flfelipelima <mota.felipelima@gmail.com>
2023-11-16 11:19:51 -0300

    Budget Items and Pages Updated

--- js/script.js
+++ js/script.js
@@ -1,12 +1,27 @@
+// Ativar links de navegação
+
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
   const url = location.href;
   const href = link.href;
-  
-  if (url.includes (href)){
+
+  if (url.includes(href)) {
     link.classList.add("ativo");
   }
 }
-
 links.forEach(ativarLink);
+
+// Itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+console.log(parametros);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
